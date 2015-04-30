69b84d9da2835fbd9a05c2ac713f1157df61775b
ziqizhang <ziqizhang.email@googlemail.com>
2015-04-30 14:37:50 +0100

    .

--- core/src/main/java/uk/ac/shef/dcs/oak/lodie/deprecated_table_list/interpreter/ModelBuilder.java
+++
@@ -1,9 +0,0 @@
-package uk.ac.shef.dcs.oak.lodie.deprecated_table_list.interpreter;
-
-/**
- * Author: Ziqi Zhang (z.zhang@dcs.shef.ac.uk)
- * Date: 28/02/13
- * Time: 14:34
- */
-public class ModelBuilder {
-}
