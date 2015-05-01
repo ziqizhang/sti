ef8e8f5a1f11523cbb9ef444fb0a4c3272a5be41
ziqizhang <ziqizhang.email@googlemail.com>
2015-05-01 16:26:05 +0100

    .

--- core/src/main/java/uk/ac/shef/dcs/oak/sti/algorithm/ji/FactorGraphBuilder.java
+++ core/src/main/java/uk/ac/shef/dcs/oak/sti/algorithm/ji/FactorGraphBuilder.java
@@ -5,9 +5,7 @@ import cc.mallet.types.LabelAlphabet;
 import uk.ac.shef.dcs.oak.sti.misc.KB_InstanceFilter;
 import uk.ac.shef.dcs.oak.sti.rep.*;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by zqz on 01/05/2015.
@@ -24,21 +22,156 @@ public class FactorGraphBuilder {
         addHeaderAndCellFactors(cellAnnotations,
                 columnHeaders,
                 annotation,
-                table, graph);
+                graph);
         //relation and pair of column types
-        addRelationAndHeaderFactors(cellAnnotations,
+        Map<String, Key_SubjectCol_ObjectCol> relationString_and_direction =
+                new HashMap<String, Key_SubjectCol_ObjectCol>();
+        Map<int[], Variable> relations = addRelationAndHeaderFactors(
                 columnHeaders,
                 annotation,
-                table, graph);
+                graph,
+                relationString_and_direction);
 
         //relation and entity pairs
+        addRelationAndCellFactors(
+                relations,
+                cellAnnotations,
+                annotation,
+                graph
+        );
+
         return graph;
+    }
+
+    private void addRelationAndCellFactors(Map<int[], Variable> relationVariables,
+                                           Map<int[], Variable> cellVariables,
+                                           LTableAnnotation_JI_Freebase annotation,
+                                           FactorGraph graph) {
+        Map<Key_SubjectCol_ObjectCol, Map<Integer, List<CellBinaryRelationAnnotation>>>
+                relations_per_row = annotation.getRelationAnnotations_per_row();
+
+        for (Map.Entry<Key_SubjectCol_ObjectCol, Map<Integer, List<CellBinaryRelationAnnotation>>>
+                entry : relations_per_row.entrySet()) {
+            Key_SubjectCol_ObjectCol relation_direction = entry.getKey();
+            Map<Integer, List<CellBinaryRelationAnnotation>> relations = entry.getValue();
+            for (Map.Entry<Integer, List<CellBinaryRelationAnnotation>> ent : relations.entrySet()) {
+                int row = ent.getKey();
+                int column1 = relation_direction.getSubjectCol();
+                int column2 = relation_direction.getObjectCol();
+
+                Variable relationVariable = relationVariables.get(new int[]{column1, column2});
+                if (relationVariable == null)
+                    relationVariable = relationVariables.get(new int[]{column2, column1});
+                Variable cellVariable1 = cellVariables.get(new int[]{row, column1});
+                Variable cellVariable2 = cellVariables.get(new int[]{row, column2});
+
+                double[] potential1 = new double[cellVariable1.getNumOutcomes() * relationVariable.getNumOutcomes()];
+                for (int i = 0; i < cellVariable1.getNumOutcomes(); i++) {
+                    for (int j = 0; j < relationVariable.getNumOutcomes(); j++) {
+                        String cellAnnotationId = cellVariable1.getLabelAlphabet().lookupLabel(i).toString();
+                        String relationURL = relationVariable.getLabelAlphabet().lookupLabel(j).toString();
+                        double score = annotation.getScore_entityAndRelation(cellAnnotationId, relationURL);
+                        potential1[i + j] = score;
+                    }
+                }
+                VarSet varSet1 = new HashVarSet(new Variable[]{cellVariable1, relationVariable});
+                TableFactor factor1 = new TableFactor(varSet1, potential1);
+                graph.addFactor(factor1);
+
+                double[] potential2 = new double[cellVariable2.getNumOutcomes() * relationVariable.getNumOutcomes()];
+                for (int i = 0; i < cellVariable2.getNumOutcomes(); i++) {
+                    for (int j = 0; j < relationVariable.getNumOutcomes(); j++) {
+                        String cellAnnotationId = cellVariable2.getLabelAlphabet().lookupLabel(i).toString();
+                        String relationURL = relationVariable.getLabelAlphabet().lookupLabel(j).toString();
+                        double score = annotation.getScore_entityAndRelation(cellAnnotationId, relationURL);
+                        potential1[i + j] = score;
+                    }
+                }
+                VarSet varSet2 = new HashVarSet(new Variable[]{cellVariable2, relationVariable});
+                TableFactor factor2 = new TableFactor(varSet2, potential2);
+                graph.addFactor(factor2);
+            }
+        }
+    }
+
+    private Map<int[], Variable> addRelationAndHeaderFactors(
+            Map<Integer, Variable> columnHeaders,
+            LTableAnnotation_JI_Freebase annotation,
+            FactorGraph graph,
+            Map<String, Key_SubjectCol_ObjectCol> reltaionString_and_direction) {
+        Map<int[], Variable> result = new HashMap<int[], Variable>();
+
+        Map<Key_SubjectCol_ObjectCol, List<HeaderBinaryRelationAnnotation>>
+                candidateRelations = annotation.getRelationAnnotations_across_columns();
+
+        List<Key_SubjectCol_ObjectCol> processed = new ArrayList<Key_SubjectCol_ObjectCol>();
+        for (Map.Entry<Key_SubjectCol_ObjectCol, List<HeaderBinaryRelationAnnotation>> entry :
+                candidateRelations.entrySet()) {
+            //the relation and its reversed-directional relation are the same variable, so collect
+            //candidates from both
+            Key_SubjectCol_ObjectCol relation_direction = entry.getKey();
+            if (processed.contains(relation_direction))
+                continue;
+
+            Key_SubjectCol_ObjectCol relation_direction_reverse = new Key_SubjectCol_ObjectCol(
+                    relation_direction.getObjectCol(), relation_direction.getSubjectCol()
+            );
+            processed.add(relation_direction);
+            processed.add(relation_direction_reverse);
+
+            List<HeaderBinaryRelationAnnotation> candidate_relations = entry.getValue();
+            List<HeaderBinaryRelationAnnotation> candidate_relations_reversed =
+                    candidateRelations.get(relation_direction_reverse);
+            if (candidate_relations_reversed != null)
+                candidate_relations.addAll(candidate_relations_reversed);
+
+            Map<int[], Double> affinity_scores_column1_and_relation = new HashMap<int[], Double>();
+            Map<int[], Double> affinity_scores_column2_and_relation = new HashMap<int[], Double>();
+            Variable column1_header_variable = columnHeaders.get(relation_direction.getSubjectCol());
+            Variable column2_header_variable = columnHeaders.get(relation_direction.getObjectCol());
+
+            //index all relation candidate for this relation variable
+            LabelAlphabet candidateIndex_relation = new LabelAlphabet();
+            for (HeaderBinaryRelationAnnotation hbr : candidate_relations) {
+                int index_relation = candidateIndex_relation.lookupIndex(hbr.getAnnotation_url(), true);
+                reltaionString_and_direction.put(hbr.getAnnotation_url(), hbr.getSubject_object_key());
+                for (int c = 0; c < column1_header_variable.getNumOutcomes(); c++) {
+                    String header_concept_url = column1_header_variable.getLabelAlphabet().lookupLabel(c).toString();
+                    double score = annotation.getScore_conceptAndRelation(header_concept_url, hbr.getAnnotation_url());
+                    if (score > 0)
+                        affinity_scores_column1_and_relation.put(new int[]{c, index_relation}, score);
+                }
+                for (int c = 0; c < column2_header_variable.getNumOutcomes(); c++) {
+                    String header_concept_url = column2_header_variable.getLabelAlphabet().lookupLabel(c).toString();
+                    double score = annotation.getScore_conceptAndRelation(header_concept_url, hbr.getAnnotation_url());
+                    if (score > 0)
+                        affinity_scores_column2_and_relation.put(new int[]{c, index_relation}, score);
+                }
+            }
+            Variable relationVariable = new Variable(candidateIndex_relation);
+            result.put(new int[]{relation_direction.getSubjectCol(),
+                    relation_direction.getObjectCol()}, relationVariable);
+
+            //create potentials
+            double[] potential1 = computePotential(affinity_scores_column1_and_relation, column1_header_variable,
+                    relationVariable);
+            VarSet varSet1 = new HashVarSet(new Variable[]{column1_header_variable, relationVariable});
+            TableFactor factor1 = new TableFactor(varSet1, potential1);
+            graph.addFactor(factor1);
+
+            double[] potential2 = computePotential(affinity_scores_column2_and_relation, column2_header_variable,
+                    relationVariable);
+            VarSet varSet2 = new HashVarSet(new Variable[]{column2_header_variable, relationVariable});
+            TableFactor factor2 = new TableFactor(varSet2, potential2);
+            graph.addFactor(factor2);
+
+        }
+        return result;
     }
 
     private void addHeaderAndCellFactors(Map<int[], Variable> cellVariables,
                                          Map<Integer, Variable> headerVariables,
                                          LTableAnnotation_JI_Freebase annotation,
-                                         LTable table,
                                          FactorGraph graph) {
         for (int row = 0; row < annotation.getRows(); row++) {
             for (int col = 0; col < annotation.getCols(); col++) {
@@ -81,10 +214,8 @@ public class FactorGraphBuilder {
     }
 
     /**
-     *
-     * @param affinity_values_between_variable_outcomes
-     *        in the key, the first element (int) must correspond to the index in cellVar; the second must
-     *        correpsond to the index in headerVar
+     * @param affinity_values_between_variable_outcomes in the key, the first element (int) must correspond to the index in cellVar; the second must
+     *                                                  correpsond to the index in headerVar
      * @param firstVar
      * @param secondVar
      * @return
@@ -95,15 +226,15 @@ public class FactorGraphBuilder {
             Variable secondVar) {
         int dimensionFirstVar = firstVar.getNumOutcomes();
         int dimensionSecondVar = secondVar.getNumOutcomes();
-        double[] res = new double[dimensionFirstVar*dimensionSecondVar];
-        for(int first = 0; first<dimensionFirstVar; first++){
-            for(int second = 0; second<dimensionSecondVar; second++){
+        double[] res = new double[dimensionFirstVar * dimensionSecondVar];
+        for (int first = 0; first < dimensionFirstVar; first++) {
+            for (int second = 0; second < dimensionSecondVar; second++) {
                 int[] key = new int[]{first, second};
                 Double affinity = affinity_values_between_variable_outcomes.get(key);
 
-                if(affinity==null)
-                     affinity=0.0;
-                res[first+second]=affinity;
+                if (affinity == null)
+                    affinity = 0.0;
+                res[first + second] = affinity;
             }
         }
         return res;
--- core/src/main/java/uk/ac/shef/dcs/oak/sti/algorithm/ji/LTableAnnotation_JI_Freebase.java
+++ core/src/main/java/uk/ac/shef/dcs/oak/sti/algorithm/ji/LTableAnnotation_JI_Freebase.java
@@ -2,16 +2,43 @@ package uk.ac.shef.dcs.oak.sti.algorithm.ji;
 
 import uk.ac.shef.dcs.oak.sti.rep.LTableAnnotation;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by zqz on 01/05/2015.
  */
 public class LTableAnnotation_JI_Freebase extends LTableAnnotation {
+
+    private Map<String, Double> score_entityAndConcept= new HashMap<String, Double>();
+    private Map<String, Double> score_entityAndRelation= new HashMap<String, Double>();
+    private Map<String, Double> score_conceptAndRelation= new HashMap<String, Double>();
     public LTableAnnotation_JI_Freebase(int rows, int cols) {
         super(rows, cols);
     }
 
     public double getScore_entityAndConcept(String entityId, String conceptId){
-        //todo
-        return 0.0;
+        Double v = score_entityAndConcept.get(createKey(entityId, conceptId));
+        if(v==null)
+            v=0.0;
+        return v;
+    }
+
+    public double getScore_conceptAndRelation(String conceptId, String relationId){
+        Double v = score_conceptAndRelation.get(createKey(conceptId, relationId));
+        if(v==null)
+            v=0.0;
+        return v;
+    }
+
+    public double getScore_entityAndRelation(String entityId, String relationId){
+        Double v = score_entityAndRelation.get(createKey(entityId, relationId));
+        if(v==null)
+            v=0.0;
+        return v;
+    }
+
+    private String createKey(String s1, String s2){
+        return s1+"|"+s2;
     }
 }
