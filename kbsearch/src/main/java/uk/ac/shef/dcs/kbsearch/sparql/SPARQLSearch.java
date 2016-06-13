97ed1e02604964e5a154f019b804ab8ca1c86840
Ziqi Zhang <ziqi.zhang@sheffield.ac.uk>
2016-06-13 16:53:53 +0100

    .

--- kbsearch/src/main/java/uk/ac/shef/dcs/kbsearch/sparql/SPARQLSearch.java
+++ kbsearch/src/main/java/uk/ac/shef/dcs/kbsearch/sparql/SPARQLSearch.java
@@ -93,6 +93,7 @@ public abstract class SPARQLSearch extends KBSearch {
     }
 
     protected String createGetLabelQuery(String content){
+        content=content.replaceAll("\\s+","");
         String query = "SELECT DISTINCT ?o WHERE {<"+
                 content+"> <"+RDFEnum.RELATION_HASLABEL.getString()+"> ?o . }";
 
