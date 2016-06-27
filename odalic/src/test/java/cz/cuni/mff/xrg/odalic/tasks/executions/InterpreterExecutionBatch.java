2bb2c92c8396dd647338ce0cdbbf6702e9daac11
Josef Janousek <pauto@centrum.cz>
2016-06-27 23:05:59 +0200

    Class for testing the execution of interpreter from Odalic project

---
+++ odalic/src/test/java/cz/cuni/mff/xrg/odalic/tasks/executions/InterpreterExecutionBatch.java
@@ -0,0 +1,60 @@
+package cz.cuni.mff.xrg.odalic.tasks.executions;
+
+import java.io.File;
+//import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import com.google.common.base.Preconditions;
+
+import uk.ac.shef.dcs.sti.STIException;
+import uk.ac.shef.dcs.sti.core.algorithm.SemanticTableInterpreter;
+import uk.ac.shef.dcs.sti.core.model.TAnnotation;
+import uk.ac.shef.dcs.sti.core.model.Table;
+//import uk.ac.shef.dcs.sti.parser.table.TableParserLimayeDataset;
+import uk.ac.shef.dcs.sti.xtractor.csv.TableXtractorCSV;
+
+public class InterpreterExecutionBatch {
+
+  public static void main(String[] args) {
+    // for test of execution of semantic table interpreter from Odalic project
+    
+    SemanticTableInterpreter semanticTableInterpreter = InterpreterFactory.getInterpreter();
+    Preconditions.checkNotNull(semanticTableInterpreter);
+    
+    //TODO: Write path of your own folder containing the input file
+    List<File> all = Arrays.asList(new File("C:\\Users\\Acer\\data-odalic\\sti-data\\Limaye200\\my").listFiles());
+    File inputFile = all.get(0);
+    
+    // code for extraction from CSV
+    TableXtractorCSV tableExtractor = new TableXtractorCSV();
+    List<Table> tables = tableExtractor.extract(inputFile, inputFile.getName());
+    
+    // code for extraction from LimayeDataset
+    /*TableParserLimayeDataset tableExtractor = new TableParserLimayeDataset();
+    List<Table> tables;
+    try {
+      tables = tableExtractor.extract(inputFile.toString(), inputFile.toString());
+    } catch (STIException e1) {
+      e1.printStackTrace();
+      tables = new ArrayList<>();
+    }*/
+    
+    if (tables.isEmpty()) {
+      throw new IllegalArgumentException();
+    }
+    
+    TAnnotation annotationResult;
+    try {
+      annotationResult = semanticTableInterpreter.start(tables.get(0), true);
+      
+      System.out.println("Výsledek - OK:");
+      System.out.println(annotationResult.toString());
+    } catch (STIException e) {
+      System.out.println("Výsledek - Chyba:");
+      e.printStackTrace();
+    }
+    System.out.println("Konec výsledku.");
+  }
+
+}
