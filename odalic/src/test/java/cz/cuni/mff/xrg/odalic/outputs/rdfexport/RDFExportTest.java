cc67ffd5b7f11251c174b69e8cba73238ed1814b
Tomas Knap <toknap@gmail.com>
2016-08-17 12:39:08 +0200

    Converted execution of main method to JUNIT test

--- odalic/src/test/java/cz/cuni/mff/xrg/odalic/outputs/rdfexport/RDFExportTest.java
+++ odalic/src/test/java/cz/cuni/mff/xrg/odalic/outputs/rdfexport/RDFExportTest.java
@@ -1,60 +1,61 @@
 package cz.cuni.mff.xrg.odalic.outputs.rdfexport;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.Reader;
-
-import org.apache.commons.io.FilenameUtils;
-import org.eclipse.rdf4j.model.Model;
-import org.eclipse.rdf4j.rio.RDFFormat;
 import com.google.gson.Gson;
-
 import cz.cuni.mff.xrg.odalic.input.CsvConfiguration;
 import cz.cuni.mff.xrg.odalic.input.DefaultCsvInputParser;
 import cz.cuni.mff.xrg.odalic.input.Input;
 import cz.cuni.mff.xrg.odalic.input.ListsBackedInputBuilder;
 import cz.cuni.mff.xrg.odalic.outputs.annotatedtable.AnnotatedTable;
+import org.apache.commons.io.FilenameUtils;
+import org.eclipse.rdf4j.model.Model;
+import org.eclipse.rdf4j.rio.RDFFormat;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.io.*;
+import java.net.URISyntaxException;
 
 public class RDFExportTest {
 
-  /**
-   * Expects annotated table JSON file path as the first and extended input CSV file path as the second
-   * command line argument
-   * 
-   * @param args command line arguments
-   * 
-   * @author Josef Janoušek 
-   */
-  public static void main(String[] args) {
-    final String jsonFilePath = args[0];
-    final String csvFilePath = args[1];
-    
+  static File inputJsonFile;
+  static File inputFile;
+
+  @BeforeClass
+  public static void beforeClass() throws URISyntaxException, IOException {
+
+    inputJsonFile = new File(RDFExportTest.class.getClassLoader().getResource("book.json").toURI());
+    inputFile = new File(RDFExportTest.class.getClassLoader().getResource("book.csv").toURI());
+  }
+
+  @Test
+  public void TestConversionToTurtle() {
+
     // Convert JSON to Java Object AnnotatedTable
     AnnotatedTable annotatedTable;
-    try (Reader reader = new FileReader(jsonFilePath)) {
+    try (Reader reader = new FileReader(inputJsonFile)) {
       annotatedTable = new Gson().fromJson(reader, AnnotatedTable.class);
     } catch (IOException e) {
       e.printStackTrace();
       return;
     }
-    
+
     // Code for extraction from CSV
-    File inputFile = new File(csvFilePath);
     Input extendedInput;
     try (final FileInputStream inputFileStream = new FileInputStream(inputFile)) {
       extendedInput = new DefaultCsvInputParser(new ListsBackedInputBuilder())
-          .parse(inputFileStream, inputFile.getName(), new CsvConfiguration());
+              .parse(inputFileStream, inputFile.getName(), new CsvConfiguration());
     } catch (IOException e) {
       e.printStackTrace();
       return;
     }
-    
+
     // Export from annotated table to RDF file
-    testExportToRDFFile(annotatedTable, extendedInput, 
-        inputFile.getParent() + File.separator + FilenameUtils.getBaseName(inputFile.getName()) + ".rdf");
+    testExportToRDFFile(annotatedTable, extendedInput,
+            inputFile.getParent() + File.separator + FilenameUtils.getBaseName(inputFile.getName()) + ".rdf");
+
+    //TODO write some assert which e.g. checks that the resulting turtle file is in the expected format
+
+
   }
   
   public static void testExportToRDFFile(AnnotatedTable annotatedTable, Input extendedInput, String filePath) {
